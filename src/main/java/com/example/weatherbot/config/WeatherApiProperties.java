a96ca7c2a7b99c2db4e3ce9ea7cf1f75389541ae
EliteHacker228 <mchappycraft@yandex.ru>
2023-03-26 05:32:49 +0500

    Move api urls to yaml config files

--- src/main/java/com/example/weatherbot/config/WeatherApiProperties.java
+++ src/main/java/com/example/weatherbot/config/WeatherApiProperties.java
@@ -7,6 +7,16 @@ import org.springframework.stereotype.Component;
 @Component
 @Getter
 public class WeatherApiProperties {
+    @Value("${weatherApi.protocol}")
+    private String protocol;
+    @Value("${weatherApi.host}")
+    private String host;
+    @Value("${weatherApi.weatherEndpoint}")
+    private String weatherEndpoint;
+    @Value("${weatherApi.forecastEndpoint}")
+    private String forecastEndpoint;
+    @Value("${weatherApi.geoEndpoint}")
+    private String geoEndpoint;
     @Value("${weatherApi.token}")
     private String token;
     @Value("${weatherApi.units}")
