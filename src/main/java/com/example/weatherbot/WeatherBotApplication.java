c5c89cb997435c1c46d3f40eae2b0ab0d28b1d82
EliteHacker228 <mchappycraft@yandex.ru>
2023-04-17 01:49:21 +0500

    Add scheduled sending functional

--- src/main/java/com/example/weatherbot/WeatherBotApplication.java
+++ src/main/java/com/example/weatherbot/WeatherBotApplication.java
@@ -3,11 +3,13 @@ package com.example.weatherbot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableAsync
 @EnableTransactionManagement
+@EnableScheduling
 public class WeatherBotApplication {
 
     public static void main(String[] args) {
